15e3a852eb4114caef8eeff5ad61ccf97dc12972
Levêque Loris Sydney <loris.leveque@epfl.ch>
2022-03-14 10:02:30 +0100

    Initial state, things should work well

---
+++ FolderComparator_cs/FoldersDiff/FoldersDiff.cs
@@ -0,0 +1,26 @@
+﻿namespace FolderComparator_cs.FoldersDiff
+{
+    internal class FoldersDiff
+    {
+        public int FilesNumberDiff { init; get; }
+        public List<string> FilesDiff { get; set; } = new List<string>();
+
+        public void SaveToFile(string path)
+        {
+            Console.WriteLine("Writing the output to: " + path);
+            using (StreamWriter writer = new StreamWriter(path))
+            {
+                writer.WriteLine(ToString());
+                writer.WriteLine(DateTime.Now);
+                writer.WriteLine("Number of files diff: " + FilesNumberDiff);
+                writer.WriteLine("List of diff files");
+
+                foreach (string file in FilesDiff)
+                {
+                    writer.WriteLine(file);
+                }
+            }
+            Console.WriteLine("Finished !");
+        }
+    }
+}
--- FolderComparator_cs/Program.cs
+++ FolderComparator_cs/Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using FolderComparator_cs.FoldersDiff;
+Console.WriteLine("Code me daddy!");
+
+const string FOLDERMAIN = "path";
+const string FOLDERSECOND = "path";
+
+const string OUTPUTPATH = "path";
+
+StartComparator();
+
+ 
+static void StartComparator()
+{
+    Console.WriteLine("Comparator Started...");
+
+    var allFilesMain = GetAllSubFiles(FOLDERMAIN);
+    var allFilesSecond = GetAllSubFiles(FOLDERSECOND);
+
+    Console.WriteLine((allFilesMain.Length + allFilesSecond.Length).ToString() + " Files founds");
+
+    var foldersDiff = new FoldersDiff() { FilesNumberDiff = Math.Abs(allFilesMain.Length - allFilesSecond.Length) };
+
+    CompareTwoFolders(ref foldersDiff, allFilesMain, allFilesSecond);
+    CompareTwoFolders(ref foldersDiff, allFilesSecond, allFilesMain);
+
+    foldersDiff.SaveToFile(OUTPUTPATH);
+}
+
+static void CompareTwoFolders(ref FoldersDiff foldersDiff, string[] allFilesMain, string[] allFilesSecond)
+{
+    foreach (var file in allFilesMain)
+    {
+        if (!ElemExistsInArray(file, allFilesSecond))
+        {
+            foldersDiff.FilesDiff.Add(file);
+        }
+    }
+}
+
+static string[] GetAllSubFiles(string path)
+{
+    return Directory.GetFiles(path, "*.*", SearchOption.AllDirectories);
+}
+
+static bool ElemExistsInArray(string elem, string[] array)
+{
+    return Array.Exists(array, cell => cell == elem);
+}
